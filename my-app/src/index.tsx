4bbea9738ef3d30e90eeb0efa3cd380fbc85f612
Melnyk Anastasiia <nastasia8811@gmail.com>
2024-04-19 23:11:26 +0200

    header

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from 'react-dom/client';
+import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from './store';
